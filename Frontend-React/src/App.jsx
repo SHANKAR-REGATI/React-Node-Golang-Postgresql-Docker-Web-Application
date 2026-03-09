e57eb306d0b4832531e80aba6a9df7f0f50f00a5
shankar_regati <reddyshankarnath@gmail.com>
2026-03-09 15:21:44 +0000

    did some file changes

--- Frontend-React/src/App.jsx
+++ Frontend-React/src/App.jsx
@@ -37,8 +37,8 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1>Hey Team! 👋</h1>
-      <CurrentTime api="/api/golang/"/>
-      <CurrentTime api="/api/node/"/>
+      <CurrentTime api="/api/golang/" />
+      <CurrentTime api="/api/node/" />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
